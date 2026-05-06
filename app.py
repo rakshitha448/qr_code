68c67f7009975fe9d7b332f032ed0d1145cd28c9
Rakshitha <rakshithavb448@gmail.com>
2026-05-06 13:48:01 +0530

    Fix indentation and improve QR code generation logic

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import streamlit as st
 import qrcode
 from PIL import Image
 
-#page title
+# page title
 st.title("Qr code generator")
 data=st.text_input("enter url")
 if st.button("Generate QR"):
-  if data:
-    qr = qrcode.make(data)
-    qr.save("qr.png")
+    if data:
+      qr = qrcode.make(data)
+      qr.save("qr.png")
 
-    image= Image.open("qr.jpg")
-   st.image(img,caption="Generated QR Code")
-
-   with open("qr.png","rb") as f:
-     st.download_button("Download QR",f,file_name="qr.png")
+      img = Image.open("qr.png")
+      st.image(img, caption="Generated QR Code")
+      
+      with open("qr.png","rb") as f:
+          st.download_button("Download QR", f, file_name="qr.png")
 else:
-  st.warning("Please enter the some text")
+   st.warning("Please enter some text")
